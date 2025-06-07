4a192bf5fcf97be2f321321aaa90b95c69b29626
H. Watanabe <kaityo@users.sourceforge.jp>
2025-06-07 15:13:03 +0900

    :memo: updates

--- examples/systeminfo/systeminfo.cpp
+++ examples/systeminfo/systeminfo.cpp
@@ -1,8 +1,15 @@
 #include <cstdio>
 #include <lammpstrj/lammpstrj.hpp>
 
-int main() {
-  lammpstrj::SystemInfo si = lammpstrj::read_info("simple.lammpstrj");
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " input.lammpstrj" << std::endl;
+    return 1;
+  }
+
+  std::string filename = argv[1];
+  lammpstrj::SystemInfo si = lammpstrj::read_info(filename);
   printf("(LX, LY, LZ) = (%f, %f, %f)\n", si.LX, si.LY, si.LZ);
   printf("N = %d\n", si.atoms);
+  return 0;
 }
